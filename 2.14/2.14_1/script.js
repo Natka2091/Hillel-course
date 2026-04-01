4cd2567b0d1ddc43ef8fa8e15839938cd7a9ac4d
Natka2091 <fomnata41@gmail.com>
2026-04-01 15:01:13 +0300

    2.14

---
+++ 2.14/2.14_1/script.js
@@ -0,0 +1,7 @@
+if (browser === 'Edge') {
+    alert ('you have got the Edge!');
+} else if (browser === ("Chrome" || "Firefox" || "Safari" || "Opera")){
+    alert ('Ми підтримуємо і ці браузери');
+} else {
+    alert ('Маємо надію, що ця сторінка виглядає добре!');
+}
---
+++ 2.14/2.14_2/script.js
@@ -0,0 +1,12 @@
+const a = prompt ('a?', '');
+switch (+a) {
+    case a === 0:
+        alert(0);
+        break;
+    case a === 1:
+        alert(1);
+        break;
+    case a === 2:
+    case a === 3:
+        alert ('2,3');
+}
